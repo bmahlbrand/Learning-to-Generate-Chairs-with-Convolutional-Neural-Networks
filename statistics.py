afc753506b9d15d587d43ed7abeea85e2ac6d9d7
wll <wllnyubupt@gmail.com>
2018-12-13 23:24:08 -0500

    change statistics

--- statistics.py
+++ statistics.py
@@ -17,30 +17,26 @@ train_image = np.asarray(train_df.iloc[:, 0])[1:]
 valid_df = pd.read_csv(valid_csv_path, header=None)
 valid_image = np.asarray(valid_df.iloc[:, 0])[1:]
 
-red = np.ones(1)
-green = np.ones(1)
-blue = np.ones(1)
+mean = 0.
+std = 0.
+nb_samples = 0.
 
 for image in train_image:
-    image_read = io.imread(image)
-    red = np.append(red, image_read[:, :, 0])
-    green = np.append(green, image_read[:,:,1])
-    blue = np.append(blue, image_read[:,:,2])
+    image = torch.from_numpy(io.imread(image).transpose((2, 0, 1)))
+    image = image.view( image.size(0), -1)
+    mean += image.float().mean(1)
+    std += image.float().std(1)
+    nb_samples += 1
 
 for image in valid_image:
-    image_read = io.imread(image)
-    red = np.append(red, image_read[:, :, 0])
-    green = np.append(green, image_read[:,:,1])
-    blue = np.append(blue, image_read[:,:,2])  
+    image = torch.from_numpy(io.imread(image).transpose((2, 0, 1)))
+    image = image.view( image.size(0), -1)
+    mean += image.float().mean(1)
+    std += image.float().std(1)
+    nb_samples += 1
 
-red = red[1:]
-green = green[1:]
-blue = blue[1:]
+mean /= nb_samples
+std /= nb_samples
 
-print("red mean: ", np.mean(red))
-print("green mean: ", np.mean(green))
-print("blue mean: ", np.mean(blue))
-
-print("red std: ", np.std(red))
-print("green std: ", np.std(green))
-print("blue mean: ", np.std(blue))
+print("mean: ", mean)
+print("std: ", std)
